ce23f96f334f229e8918a63b0e930bcb6d5d0277
Daniel <github.danimoh@neverbox.com>
2019-12-03 09:48:28 -0600

    Setup linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,50 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-    '@vue/typescript',
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  },
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
-      env: {
-        jest: true,
-      },
+    root: true,
+    env: {
+        browser: true,
     },
-  ],
+    extends: [
+        'plugin:vue/essential',
+        '@vue/airbnb',
+        '@vue/typescript',
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        indent: ['error', 4, { SwitchCase: 1 }],
+        'max-len': ['error', 120],
+        'no-underscore-dangle': 'off',
+        'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+        'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
+        'object-curly-newline': ['error', { ObjectPattern: { multiline: true } }],
+        'prefer-const': ['error', { destructuring: 'all' }],
+        'no-nested-ternary': 'off',
+
+        // Typescript plugin replacements
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'error',
+        'no-useless-constructor': 'off',
+        '@typescript-eslint/no-useless-constructor': 'error',
+        'no-empty-function': 'off',
+        '@typescript-eslint/no-empty-function': 'error',
+
+        // False positives that are checked by TS
+        'no-redeclare': 'off',
+        'no-param-reassign': 'off',
+        'import/no-unresolved': 'off',
+    },
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+    },
+    overrides: [
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)',
+            ],
+            env: {
+                jest: true,
+            },
+        },
+    ],
 };
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,10 +8,38 @@
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
+      <li>
+          <a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
+              target="_blank"
+              rel="noopener"
+          >
+              babel
+          </a>
+      </li>
+      <li>
+          <a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
+              target="_blank"
+              rel="noopener"
+          >
+              typescript
+          </a>
+      </li>
+      <li>
+          <a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
+              target="_blank"
+              rel="noopener"
+          >
+              eslint
+          </a>
+      </li>
+      <li>
+          <a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest"
+              target="_blank"
+              rel="noopener"
+          >
+              unit-jest
+          </a>
+      </li>
     </ul>
     <h3>Essential Links</h3>
     <ul>
@@ -25,7 +53,14 @@
     <ul>
       <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
       <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
+      <li>
+          <a href="https://github.com/vuejs/vue-devtools#vue-devtools"
+              target="_blank"
+              rel="noopener"
+          >
+              vue-devtools
+          </a>
+      </li>
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
