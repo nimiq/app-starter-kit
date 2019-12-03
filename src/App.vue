3d0b2d30641cdf9b405cc7c70ed4c3668004f494
Daniel <github.danimoh@neverbox.com>
2019-12-03 11:45:45 -0600

    Setup initial demo

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <div id="app">
+        <h1>Hi!</h1>
+
+        <p>Congratulations, you just set up a fully functional, Nimiq powered app.</p>
+        <p>This app template already contains everything you need to build an awesome app.</p>
+
+        <ProjectInfo />
+
+        <WhatNextInfo />
+    </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import ProjectInfo from './components/ProjectInfo.vue';
+import WhatNextInfo from './components/WhatNextInfo.vue';
 
-@Component({
-    components: {
-        HelloWorld,
-    },
-})
+@Component({ components: { ProjectInfo, WhatNextInfo } })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
