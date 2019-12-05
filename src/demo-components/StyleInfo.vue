64246883446aa3b3d10efca73a1240917cd69ba0
Daniel <github.danimoh@neverbox.com>
2019-12-04 21:05:45 -0600

    Setup @nimiq/style
    Also adding Muli and Fira Mono fonts to blocking.css

---
+++ src/demo-components/StyleInfo.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="nq-card">
+        <div class="nq-card-header">
+            <h2 class="nq-h2">Nimiq Styling</h2>
+        </div>
+        <div class="nq-card-body">
+            <p class="nq-text">
+                This app template includes the default
+                <a href="https://github.com/nimiq/nimiq-style" target="_blank" rel="noopener">styling framework</a>
+                of Nimiq Ecosystem apps which follows the
+                <a href="https://www.nimiq.com/styleguide" target="_blank" rel="noopener">Nimiq Styleguide</a>.
+                <br>
+                See
+                <a href="https://nimiq.github.io/nimiq-style/demo.html" target="_blank" rel="noopener">
+                    here for a complete demo </a>.
+            </p>
+            <p class="nq-text">
+                Additionally,
+                <a href="https://fonts.google.com/specimen/Muli" target="_blank" rel="noopener">Muli</a>
+                is included as default font and a subset of
+                <a href="https://fonts.google.com/specimen/Fira+Mono" target="_blank" rel="noopener">Fira Mono</a>
+                for display of Nimiq Addresses.
+            </p>
+            <p class="nq-text">
+                The Nimiq style framework is contained in the
+                <em>nimiq-style</em>
+                branch.
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'StyleInfo',
+};
+</script>
