756417d4de2277052ad580883f1979ef10bea73d
Daniel <github.danimoh@neverbox.com>
2020-02-24 06:31:47 +0100

    Demo Wallet Step 6: display transaction status

--- src/demo-components/DemoWallet.vue
+++ src/demo-components/DemoWallet.vue
@@ -14,7 +14,7 @@
             <button class="nq-button" :disabled="!recipientAddress || !amount" @click="sendTransaction">Send</button>
         </PageBody>
         <PageBody v-else-if="state === constructor.State.TRANSACTION_STATUS">
-            <!-- Show status of sent transaction -->
+            <p class="nq-text">Your transaction is: {{transactionState}}</p>
         </PageBody>
         <PageFooter class="nq-text">
             {{peerCount}} peers -
@@ -47,6 +47,9 @@ class DemoWallet extends Vue {
     private recipientAddress: string = '';
     private amount: number = 0;
 
+    private transactionHash: string = '';
+    private transactionState: string = 'unknown';
+
     private async created() {
         // Get the Network client singleton. If you need to configure the client, use NetworkClient.createInstance
         const networkClient = NetworkClient.Instance;
@@ -55,11 +58,17 @@ class DemoWallet extends Vue {
         networkClient.on(NetworkClient.Events.PEER_COUNT, (peerCount) => this.peerCount = peerCount);
         networkClient.on(NetworkClient.Events.CONSENSUS, (consensusState) => this.consensusState = consensusState);
         networkClient.on(NetworkClient.Events.HEAD_HEIGHT, (height) => this.blockchainHeight = height);
+        networkClient.on(NetworkClient.Events.TRANSACTION, (transaction) => {
+            if (transaction.transactionHash !== this.transactionHash) return; // ignore other transactions
+            ({ state: this.transactionState } = transaction);
+        });
     }
 
     private async chooseAddress() {
         ({ address: this.myAddress } = await this.hubClient.chooseAddress({ appName: 'Demo Wallet' }));
         this.state = DemoWallet.State.TRANSACTION_DETAILS;
+        await this.networkReadyPromise;
+        NetworkClient.Instance.subscribe([this.myAddress]); // subscribe to updates and transactions for this address
     }
 
     private async sendTransaction() {
@@ -76,14 +85,15 @@ class DemoWallet extends Vue {
             });
         }
 
-        const { serializedTx } = await this.hubClient.signTransaction({
+        const signedTransaction = await this.hubClient.signTransaction({
             appName: 'Demo Wallet',
             sender: this.myAddress,
             recipient: this.recipientAddress,
             value: this.amount,
             validityStartHeight: this.blockchainHeight,
         });
-        networkClient.sendTransaction(serializedTx);
+        this.transactionHash = signedTransaction.hash;
+        networkClient.sendTransaction(signedTransaction.serializedTx);
         this.state = DemoWallet.State.TRANSACTION_STATUS;
     }
 }
